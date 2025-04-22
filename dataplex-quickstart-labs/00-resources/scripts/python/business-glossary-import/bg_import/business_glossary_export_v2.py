c88137eb61557a4b3484638d0882e9436030ce0a
Greeshma Amaraneni <82516143+Greeshma-03@users.noreply.github.com>
2025-04-23 03:44:08 +0530

    Update flags (#65)
    * Updated input flags and changed code for better error experience
    
    * Updated prod project number
    
    * Added staging supoort

--- dataplex-quickstart-labs/00-resources/scripts/python/business-glossary-import/bg_import/business_glossary_export_v2.py
+++ dataplex-quickstart-labs/00-resources/scripts/python/business-glossary-import/bg_import/business_glossary_export_v2.py
@@ -21,8 +21,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 logger = logging_utils.get_logger()
 
-# Project number pointing to prod for export 
-PROJECT_NUMBER = "418487367933"
 MAX_WORKERS = 20
 
 def get_entry_type_name(entry_type: str) -> str:
@@ -286,7 +284,7 @@ def export_glossary_entries_json(entries: List[Dict[str, Any]],
             for future in as_completed(futures):
                 result = future.result()
                 if result:
-                    outputfile.write(json.dumps(result, indent=4) + "\n")
+                    outputfile.write(json.dumps(result) + "\n")
 
 
 def export_entry_links_json(entries: List[Dict[str, Any]], relationships_data: Dict[str, List[Dict[str, Any]]], output_json: str):
@@ -311,21 +309,26 @@ def export_entry_links_json(entries: List[Dict[str, Any]], relationships_data: D
                         filtered_links.append(link)  # Store the unique link in a list
 
     with open(output_json, mode="w", encoding="utf-8") as outputfile:
-        json.dump(filtered_links, outputfile, indent=4)
-
+        for link in filtered_links:
+            outputfile.write(json.dumps(link) + "\n")
 
 
 def main():
     args = utils.get_export_v2_arguments()
     utils.validate_export_v2_args(args)
 
-    global DATAPLEX_ENTRY_GROUP, PROJECT, LOCATION, GLOSSARY
+    global DATAPLEX_ENTRY_GROUP, PROJECT, LOCATION, GLOSSARY, PROJECT_NUMBER, DATACATALOG_BASE_URL
     PROJECT = args.project
     LOCATION = args.location
     GLOSSARY = args.glossary
-    DATAPLEX_ENTRY_GROUP = f"projects/{args.project}/locations/{args.location}/entryGroups/@dataplex"
-    
+    DATAPLEX_ENTRY_GROUP = f"projects/{PROJECT}/locations/{LOCATION}/entryGroups/@dataplex"
+    if args.testing:
+        PROJECT_NUMBER = "418487367933"  # Staging project number
+    else:
+        PROJECT_NUMBER = "655216118709"  # Prod project number
+    logger.info("Fetching entries in the Glossary...")
     entries = utils.fetch_entries(args.project, args.location, args.group)
+    logger.info("Fetching entry links in the Glossary...")
     relationships_data = utils.fetch_all_relationships(entries, args.project)
     map_entry_id_to_entry_type = {get_entry_id(e["name"]): e.get("entryType", "") for e in entries}
     parent_mapping = build_parent_mapping(entries, relationships_data)
@@ -348,4 +351,12 @@ if __name__ == "__main__":
     start_time = time.time()
     main()
     end_time = time.time()
-    logger.info(f"Program finished in {end_time - start_time} seconds")
+    elapsed_time = end_time - start_time
+    hours, remainder = divmod(int(elapsed_time), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    if hours > 0:
+        logger.info(f"Export completed in {hours} hr {minutes} min {seconds} sec")
+    elif minutes > 0:
+        logger.info(f"Export completed in {minutes} min {seconds} sec")
+    else:
+        logger.info(f"Export completed in {seconds} sec")
